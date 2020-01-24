fb4006251c4cfb71ed62195ce9e97918fa55ca9b
Lubomir Stankov <lubomir.c.stankov@gmail.com>
2020-01-24 18:36:19 +0200

    Changing Database cursor

--- index.py
+++ index.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask import jsonify
-from database import Database
+from flask_cors import CORS
+from dbhelpers import DBHelpers
+
 import json
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-@app.route('/api/user/<int:userID>/notes', methods=['GET'])
+
+@app.route('/api/v1/user/<int:userID>/notes', methods=['GET'])
 def index(userID):
-    db = Database()
-    user_notes = db.list_user_notes(userID)
+    user_notes = DBHelpers().list_user_notes(userID)
 
     return jsonify(user_notes)
