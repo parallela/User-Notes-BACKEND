ed667fa1bd6f2fdd1430e668ddb45fecf762aa68
Lubomir Stankov <lubomir.c.stankov@gmail.com>
2020-01-25 20:45:10 +0200

    Implemented JWT authentication

--- dbhelpers.py
+++ dbhelpers.py
@@ -5,15 +5,29 @@ class DBHelpers:
     def __init__(self):
         self.database = Database()
         self.mysql = self.database.connection()
+        self.dbprefix = os.getenv('DATABASE_PREFIX')
 
     def list_user_notes(self,user_id):
-        self.mysql.execute("SELECT * FROM {}notes WHERE user_id={}".format(os.getenv('DATABASE_PREFIX'),int(user_id)))
+        self.mysql.execute("SELECT * FROM {}notes WHERE user_id={}".format(self.dbprefix,int(user_id)))
         result = self.mysql.fetchall()
 
         return result
 
-    def see_note(self,note_slug):
-        self.mysql.execute("SELECT * FROM {}notes WHERE slug='{}'".format(os.getenv('DATABASE_PREFIX'),str(note_slug)))
+    def see_note(self,user_id,note_slug):
+        self.mysql.execute("SELECT * FROM {}notes WHERE slug='{}' AND user_id={}".format(self.dbprefix,str(note_slug),int(user_id)))
         result = self.mysql.fetchone()
 
-        return result 
+        return result
+
+    def get_user(self,email):
+        self.mysql.execute("SELECT * FROM {}users WHERE email='{}'".format(self.dbprefix, str(email)))
+        result = self.mysql.fetchone()
+        
+
+        return result
+
+    def get_user_data(self,user_id):
+        self.mysql.execute("SELECT id,username,created_at,email FROM {}users WHERE id={}".format(self.dbprefix,int(user_id)))
+        result = self.mysql.fetchone()
+
+        return result       
--- index.py
+++ index.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
-from flask import jsonify
-import json
+from flask import jsonify, abort, request, json
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token,get_jwt_identity, jwt_refresh_token_required, create_refresh_token
 from dbhelpers import DBHelpers
 #logging
 from pprint import pprint
@@ -9,22 +11,62 @@ from pprint import pprint
 import json
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+jwt = JWTManager(app)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-
-@app.route('/api/v1/user/<int:userID>/notes', methods=['GET'])
-def index(userID):
-    user_notes = DBHelpers().list_user_notes(userID)
+@app.route('/api/v1/user/notes', methods=['GET'])
+@jwt_required
+def index():
+    user = get_jwt_identity()
+    user_notes = DBHelpers().list_user_notes(user['userid'])
     
     return jsonify(user_notes)
 
-# @app.route('/api/v1/show/note/<slug>', methods=['GET'])
-# def show_note(slug):
-#     note = DBHelpers().see_note(slug)
-#     json_dict = json.loads(note)
+@app.route('/api/v1/user/<int:userID>/note/<slug>', methods=['GET'])
+def show_note(userID,slug):
+    note = DBHelpers().see_note(userID,slug)
+    
+    if note['public'] != 1:
+        abort(404, description="Resource not found, or its private")
 
-#     for note in json_dict:
-#         if(note['public'] == 0):
-#             return jsonify(public=false)
+    return jsonify(note)
 
-#     return jsonify(note)
+@app.route('/api/v1/user/login',methods=['POST'])
+def login_user():
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+    result = None
+
+    user = DBHelpers().get_user(str(email))
+
+    if user == None:
+        result = jsonify({"error": "User not found"}),401
+
+    if user['verified'] == 1:         
+        if bcrypt.check_password_hash(user['password'], password):
+            token = create_access_token(identity={"userid":user['id']})
+            refresh_token = create_refresh_token(identity={"userid":user['id']})
+            result = jsonify({"token":token,"refresh_token":refresh_token})
+        else:
+            result = jsonify({"error": "Invalid credentials"}),401
+    else:
+        result = jsonify({"error":"User not verified"}),403           
+    return result
+
+@app.route('/api/v1/user/refresh',methods=['POST'])
+@jwt_refresh_token_required
+def refresh_user_token():
+    user = get_jwt_identity()
+    token = create_access_token(identity={"userid":user['userid']})
+
+    return jsonify({'token': token}), 200
+    
+@app.route('/api/v1/user')
+@jwt_required
+def user():
+    user = get_jwt_identity()
+    result = DBHelpers().get_user_data(user['userid'])
+
+    return jsonify(result)
